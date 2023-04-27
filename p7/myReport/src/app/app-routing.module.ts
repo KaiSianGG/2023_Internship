b3cd0b24fb9d8cef1931b37527a1d857ffdaf1be
KaiSianGG <ngkaisian.ksn@gmail.com>
2023-04-27 13:47:53 +0800

    work 7 angular project

--- p7/myReport/src/app/app-routing.module.ts
+++ p7/myReport/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstReportComponent } from './first-report/first-report.component';
 import { SecondReportComponent } from './second-report/second-report.component';
 
+// 設計網址路徑，就可以順利的開啓自己想要的網頁
 const routes: Routes = [
-    {path: '', component:FirstReportComponent},
-    {path: '2', component:SecondReportComponent}
+    { path: '', component: FirstReportComponent },
+    { path: '2', component: SecondReportComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- p7/myReport/src/app/app.module.ts
+++ p7/myReport/src/app/app.module.ts
@@ -7,16 +7,16 @@ import { FirstReportComponent } from './first-report/first-report.component';
 import { SecondReportComponent } from './second-report/second-report.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FirstReportComponent,
-    SecondReportComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FirstReportComponent, // 新增新的元件后會自動添加到這裏
+        SecondReportComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
